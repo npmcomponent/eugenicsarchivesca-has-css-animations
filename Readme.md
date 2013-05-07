d37a6d67a239e5d13a9ee0d70dd8ebd79cba2579
Ben McMahen <ben.mcmahen@gmail.com>
2013-05-07 15:45:21 -0600

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# has-css-animations
+
+  determines if the browser supports css animations (basically, IE 9 and below)
+
+## Installation
+
+    $ component install eugenicsarchivesca/has-css-animations
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,23 @@
+// https://developer.mozilla.org/en-US/docs/CSS/Tutorials/Using_CSS_animations/Detecting_CSS_animation_support
+
+var animation = false,
+    animationstring = 'animation',
+    keyframeprefix = '',
+    domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
+    pfx  = '';
+
+if( elm.style.animationName ) { animation = true; }
+
+if( animation === false ) {
+  for( var i = 0; i < domPrefixes.length; i++ ) {
+    if( elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
+      pfx = domPrefixes[ i ];
+      animationstring = pfx + 'Animation';
+      keyframeprefix = '-' + pfx.toLowerCase() + '-';
+      animation = true;
+      break;
+    }
+  }
+}
+
+module.exports = animation;
